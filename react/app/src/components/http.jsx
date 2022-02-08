97f58383616e712d547fdad8d16f18a10d46da64
marnysan111 <o-masato@outlook.jp>
2022-02-08 15:41:08 +0000

    不明

--- react/app/src/components/http.jsx
+++ react/app/src/components/http.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import {Box, Grid} from '@material-ui/core';
 
-
 export default function HTTP() {
+    const handleSubmit = e => {
+        e.preventDefault();
+        alert("a")
+    }
+
     return (
         <React.Fragment>
             <Box m={3}>
                 <h2>HTTP通信</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <Grid container>
                         <Grid item xs={12}>
                             送信先
@@ -18,7 +22,9 @@ export default function HTTP() {
                         <Grid item xs={12}>
                             回数
                         </Grid>
+                        <button type="submit">送信</button>
                     </Grid>
+
                 </form>
             </Box>
         </React.Fragment>
---
+++ react/app/src/components/inputIPadd.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import 'react-tabs/style/react-tabs.css';
+import {Box} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
+import MaskedInput from "react-text-mask";
+import '../App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+export default function InputIP(props) {
+  const classes = useStyle();
+    const IPAdd = {
+        guide: true,
+        mask: value => {
+          let result = [];
+          const chunks = value.split(".");
+      
+          for (let i = 0; i < 4; ++i) {
+            const chunk = (chunks[i] || "").replace(/_/gi, "");
+      
+            if (chunk === "") {
+              result.push(/\d/, /\d/, /\d/, ".");
+              continue;
+            } else if (+chunk === 0) {
+              result.push(/\d/, ".");
+              continue;
+            } else if (
+              chunks.length < 4 ||
+              (chunk.length < 3 && chunks[i].indexOf("_") !== -1)
+            ) {
+              if (
+                (chunk.length < 2 && +`${chunk}00` > 255) ||
+                (chunk.length < 3 && +`${chunk}0` > 255)
+              ) {
+                result.push(/\d/, /\d/, ".");
+                continue;
+              } else {
+                result.push(/\d/, /\d/, /\d/, ".");
+                continue;
+              }
+            } else {
+              result.push(...new Array(chunk.length).fill(/\d/), ".");
+              continue;
+            }
+          }
+      
+          result = result.slice(0, -1);
+          return result;
+        },
+        pipe: value => {
+          if (value === "." || value.endsWith("..")) return false;
+      
+          const parts = value.split(".");
+      
+          if (
+            parts.length > 4 ||
+            parts.some(part => part === "00" || part < 0 || part > 255)
+          ) {
+            return false;
+          }
+          return value;
+        }
+      };
+
+    
+    
+    return (
+        <React.Fragment>
+            <Box m={1}>
+              <label className={classes.label}>IPアドレス</label>
+              <div className={classes.block}>
+              <label className={classes.input}>送信元</label><MaskedInput {...IPAdd} id="srcIP" className={classes.input}/>
+              </div>
+            </Box>
+        </React.Fragment>
+    )
+}
+
+
+const useStyle = makeStyles(() => ({
+  label: {
+      marginRight: "60px",
+      fontWeight: "bold",
+
+  },
+  input: {
+    marginRight: "20px",
+  },
+  block: {
+    display: "inline-block",
+    whiteSpace: "nowrap",
+  }
+}))
+
