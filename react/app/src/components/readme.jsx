9d5802172680fd4ec8a2be574e131d471a061362
marnysan111 <o-masato@outlook.jp>
2022-02-17 11:33:44 +0000

    readme

--- react/app/src/components/readme.jsx
+++ react/app/src/components/readme.jsx
@@ -7,7 +7,8 @@ export default function Readme() {
         <React.Fragment>
             <Box m={3}>
                 <h2>README</h2>
-                HTTP、TCP、SYNの使い方について
+                これはパケットを生成し送信を行うツールです。<br />
+                本ツールを用いて犯罪行為や憲法違反になるような行為が発生した場合、本ツール作成者は一切の責任を負いかねます。
             </Box>
         </React.Fragment>
     )
